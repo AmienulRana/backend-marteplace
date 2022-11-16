bb35a90df7a6a467b38f0a5099a74612a2f3cc96
AmienulRana <amienulrana@gmail.com>
2022-11-16 13:08:01 +0700

    menambahkan nilai yang akan mengisi key dari mongodb_url jika mode !== dev

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const CONFIG = {
-  mongodb_url: process.env.MODE === "dev" ? process.env.MONGODB_URL_DEV : "",
+  mongodb_url: process.env.MODE === "dev" ? process.env.MONGODB_URL_DEV : process.env.MONGODB_URL_DEV,
   port: process.env.PORT,
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
