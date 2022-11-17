0ec1d5b348d14e5d7cb8ec2764843e31a0275354
AmienulRana <amienulrana@gmail.com>
2022-11-17 10:48:31 +0700

    menghapus object yang tidak gunakan di variable CONFIG

--- config/index.js
+++ config/index.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const CONFIG = {
-  mongodb_url: process.env.MODE === "dev" ? process.env.MONGODB_URL_DEV : process.env.MONGODB_URL_DEV,
+  mongodb_url: process.env.MODE === "dev" ? process.env.MONGODB_URL_DEV : process.env.MONGODB_URL_PROD,
   port: process.env.PORT,
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  refresh_token: process.env.GDRIVE_REFRESH_TOKEN,
 };
 
 module.exports = CONFIG;
